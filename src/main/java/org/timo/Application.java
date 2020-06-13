0d4ea6ccac6ac1db880a9da315ba2ad6cc936dbf
markers <lleeyj94@gmail.com>
2020-06-13 23:58:32 +0900

    setup gradle

---
+++ src/main/java/org/timo/Application.java
@@ -0,0 +1,12 @@
+//Application.java
+package org.timo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String arg[]){
+        SpringApplication.run(Application.class, arg);
+    }
+}
